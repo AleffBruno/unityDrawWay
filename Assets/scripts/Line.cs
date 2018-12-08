9a1a230225e76b23256367ea6ab1cef9175f1467
Aleff Bruno <aleffbruno@gmail.com>
2018-12-08 15:03:30 -0200

    before install sprite shape

---
+++ Assets/scripts/Line.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+	public LineRenderer lineRenderer;
+	public EdgeCollider2D edgeCol;
+
+	List<Vector2> points; 
+
+	void Start() {
+		
+		//print(v2);
+	}
+
+	void Update() {
+
+
+	}
+
+
+	public void UpdateLine (Vector2 mousePos) {
+		if (points == null) {
+			points = new List<Vector2>();
+			SetPoints(mousePos);
+			return;
+		}
+
+		if (Vector2.Distance(points.Last(),mousePos) > .1f) {
+			SetPoints(mousePos);
+		}
+	}
+
+	public void SetPoints (Vector2 point) {
+		points.Add(point);
+
+		lineRenderer.numPositions = points.Count;
+		//lineRenderer.positionCount = points.Count; //the above is obsolete, this is the current
+		lineRenderer.SetPosition(points.Count -1,point);
+
+		if (points.Count > 1) {
+			edgeCol.points = points.ToArray();
+		}
+	}
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	//30:35
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			rb.bodyType = RigidbodyType2D.Dynamic;
+		}
+	}
+}
